9a55cc226fbed6af0be6d104a599dff39724ec03
karmana13 <55777540+karmana13@users.noreply.github.com>
2019-10-02 16:46:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,38 +2,33 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-module.exports = router;
-
-router.get('/mongodb', function (req, res, next) {
-    MongoClient.connect("mongodb://karmana:Heroku-karmana1@ds229088.mlab.com:29088/heroku_nb76xh5b", function(err, myClient) {
-        if(err) throw err;
-        //get collection of routes
-        const randomDatabase = myClient.db('heroku_nb76xh5b');
-        var Routes = randomDatabase.collection('Routes');
-        //get all Routes with frequency >=1
-        Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
-            if(err) throw err;
-
-            res.render('pages/mongodb', {results: docs});
-
-        });
-
-        //close connection when your app is terminating.
-        myClient.close(function (err) {
-            if(err) throw err;
-        });
-    });//end of connect
-});//end app.get
+const mongodb = require('mongodb').MongoClient;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/mongodb', function (request, response) {
+
+    mongodb.connect(process.env.MONGODB_URI, function(err, client) {
+        if(err) throw err;
+        //get collection of routes
+        var db = client.db('heroku_nb76xh5b');
+        var Routes = db.collection('Routes');
+        //get all Routes with frequency >=1
+        Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('pages/mongodb', {results: docs});
+
+        });
+
+        //close connection when your app is terminating.
+        client.close(function (err) {
+            if(err) throw err;
+        })
+    })//end of connect
+})//end app.get
+ 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
